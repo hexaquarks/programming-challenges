4f314a2beb5bedd15ed20ede8a1ca5e8b1b6826e
hexaquarks <seseriusca@gmail.com>
2022-03-26 22:39:32 -0400

    1- Two Sum : Easy

---
+++ Misc/1_Two_Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mem;
+        vector<int> ans;
+        
+        int i = 0;
+        while (i < nums.size()) {
+            if(mem.find(target - nums[i]) != mem.end()) {
+                ans.push_back(mem[target - nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            else mem[nums[i]]=i;
+            ++i;
+        }
+        return ans;
+    }
+};
