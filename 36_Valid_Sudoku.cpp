326507f1fe9a8cb6bc423708492a7970d5028acc
hexaquarks <seseriusca@gmail.com>
2022-06-24 16:22:36 -0400

    Added information for comprehension

--- 36_Valid_Sudoku.cpp
+++ 36_Valid_Sudoku.cpp
@@ -8,25 +8,28 @@ class Solution
 public:
     bool isValidSudoku(vector<vector<char>>& board) 
     {
-        vector<set<int>> rows(9), cols(9), blocks(9);
+        vector<set<int>> cols(9), rows(9), blocks(9);
+        // std::set::count(x) is a function that counts the number
+        // of times a value x is present in the set, it can only return 
+        // 1 or 0 as the set elements are unique by definitin
         
-        for (int i = 0; i < 9; ++i) 
+        for (int i = 0; i < 9; ++i)
         {
-            for (int j = 0; j < 9; ++j) 
+            for (int j = 0; j < 9; ++j)
             {
-                
-                if (board[i][j] == '.') continue;
-                
-                int curr = board[i][j] - '0';
-                if (rows[i].count(curr) || cols[j].count(curr) ||               blocks[(i/3)*3+j/3].count(curr)) 
+                int val = board[i][j] - '0'; // this converts a 256 char to an int
+                if (rows[i].count(val) || cols[j].count(val) ||
+                    blocks[(i/3)*3 + j/3].count(val))
+                {
+                    // to find the k^th box given i in [0,8] and j in [0,8], 
+                    // there exists the shortcut (i/3)*3 + (j/3). This trick 
+                    // holds here since for any (i,j) pair there is a unique 
+                    // element mapped within K = {k_0,...,k_8}.
                     return false;
-                
-                rows[i].insert(curr);
-                cols[j].insert(curr);
-                blocks[(i/3)*3+j/3].insert(curr);
-            }
+                }
+            }    
         }
         
-        return true;
+        return true; 
     }
 };
