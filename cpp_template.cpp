bf622f38040d5aca1814482ba357b5678f72c413
hexaquarks <seseriusca@gmail.com>
2022-11-25 13:16:50 -0500

    Add a c++ template for solving Kattis

---
+++ cpp_template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0)
+#define ll long long
+#define f first
+#define s second
+#define endl "\n"
+
+template<typename T>
+void printVector(const T& t) {
+    std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
+}
+
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+
+int main()
+{
+    IOS;
+    
+
+    return 0;
+}
