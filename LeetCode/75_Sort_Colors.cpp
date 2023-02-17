31fcdb66be5140e17468c799ddc22440696425e8
Mihail Anghelici <seseriusca@gmail.com>
2023-02-17 17:06:59 -0500

    Add one more Leetcode solution

---
+++ LeetCode/75_Sort_Colors.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        for (int i = 0; i <= r; ) {
+            if (nums[i] == 0) {
+                swap(nums[i], nums[l]);
+                ++l; 
+                ++i;
+            } else if (nums[i] == 1) ++i;
+            else {
+                swap(nums[i], nums[r]);
+                --r;
+            }
+        }
+    }
+};
