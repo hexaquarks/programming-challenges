359be46d90fb129497f8011da6d5f20c76c357c8
hexaquarks <seseriusca@gmail.com>
2023-12-08 13:32:35 -0500

    Add AOC 2023 day 4 part b solution

--- AdventOfCode/2023/day4.rs
+++ AdventOfCode/2023/day4.rs
@@ -24,6 +24,49 @@ fn solution_day4a(filename: &str) -> i32 {
     result
 }
 
+fn solution_day4b(filename: &str) -> i32 {
+    let mut result = 0;
+    let file_contents = read_to_string(filename).unwrap();
+    let mut card_counts: Vec<usize> = Vec::new();
+
+    for line in file_contents.lines() {
+        let split: Vec<&str> = line.split(": ").collect();
+        let win_data_split: Vec<&str> = split[1].split(" | ").collect();
+        let original_vals: HashSet<&str> = win_data_split[0].split_whitespace().collect();
+
+        let matches = win_data_split[1]
+            .trim()
+            .split_whitespace()
+            .filter(|&val| original_vals.contains(val))
+            .count();
+
+        // Add the original card
+        card_counts.push(matches);
+    }
+
+    let mut updated_card_counts: Vec<usize> = vec![1; card_counts.len()];
+
+    for (idx, &matches) in card_counts.iter().enumerate() {
+        let curr_val = updated_card_counts[idx];
+        result += curr_val;
+
+        if idx == card_counts.len() || matches == 0 {
+            continue;
+        }
+
+        for _ in 0..curr_val {
+            for offset in 1..=matches {
+                let new_idx = idx + offset;
+                if (new_idx < card_counts.len()) {
+                    updated_card_counts[new_idx] += 1;
+                }
+            }
+        }
+    }
+    result as i32
+}
+
 fn main_entry() {
-    println!("{}", solution_day4a("src/day4_input.txt"));
+    println!("{}", solution_day4a("src/day4_input.txt"))
+    println!("{}", solution_day4b("src/day4_input.txt"))
 }
